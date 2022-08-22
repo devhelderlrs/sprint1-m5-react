8ac5f1d093ae1fa376c9b0fcacfd3880ea265988
devhelderlrs <helderl4004@gmail.com>
2022-08-22 19:53:32 -0300

    grid alterado

--- src/Email.jsx
+++ src/Email.jsx
@@ -2,6 +2,7 @@ import {HeaderEmail} from "./components/header/Header.jsx"
 import {ParagrafoEmail} from "./components/paragraphs/Paragraphs.jsx"
 import GridCardsEmail from "./components/Cards/GridCards.jsx"
 import Footer from "./components/footer/Footer.jsx"
+import './app.css'
 
 export default function Email() {
   return (
